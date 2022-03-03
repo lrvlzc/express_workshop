0cd6c8c60b9f615e68523e89df66f5a67761e3a1
vzcl <lvelazquez08@alumnos.uaq.mx>
2022-03-02 19:26:15 -0600

    initial comit

--- index.js
+++ index.js
@@ -4,52 +4,60 @@
 //let y = 0; var z = 0;
 
 const express =require("express"); //llama a la libreria express
+const res = require("express/lib/response");
 const app = express();
 const { pokemon } = require('./pokedex.json');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 /*los verbos hhtp 
 GET-obtener un recurso
-POST-guardar o publicar
-PATH- actualización-un dato de un recurso
+POST-guardar/ crear recursos
+PATCH- actualización-un dato de un recurso
 PUT-actualización-todos los elementos
 DELETE-eliminar recursos
 */
 
+
+
 app.get("/", (req, res, next) =>{ //req-petición, res-respuesta, next-
-    res.status(200);
-    res.send("Bienvenido al Pokedex");
+    return res.status(200).send("Bienvenido al Pokedex");
 });
 
-app.get("/pokemon/all", (req, res, next) =>{ // "/:name", dos puntos para crear variable
+app.post("/pokemon", (req,res,next)=>{
+    return res.status(200).send(req.body.name);
+})
+
+
+app.get("/pokemon/", (req, res, next) =>{ // "/:name", dos puntos para crear variable
     //console.log(req.params.name);
-    res.status(200);
-    res.send(pokemon);
+    return res.status(200).send(pokemon);
 }); //obtener datos del usuario apartir de la URL
 
 app.get('/pokemon/:id([0-9]{1,3})', (req,res,next)=>{ //([0-9]{1,3})funcion para solo aceptar numeros, [0-9] acepta digitos del 0 al 9, {1,3} acepta numeros entre 1 a 3 digitos
     const id = req.params.id - 1;
-    if(id >= 0 && id <= 150){
-        res.status(200);
-        return res.send(pokemon[req.params.id - 1]);
-    }
-    //else{
-        res.status(404);
-        res.send("Pokémon no encontrado");
-    //}
-
+    (id >= 0 && id <= 150) ?
+        res.status(200).send(pokemon[req.params.id - 1]):
+        res.status(404).send("Pokémon no encontrado");
 });
 
-app.get('/pokemon/:name', (req, res, next)=>{
+app.get('/pokemon/:name([A-Za-z]+)', (req, res, next)=>{
     const name = req.params.name;
-    for(i =0; i<pokemon.length;i++){
-        if(pokemon[i].name==name){
-            res.status(200);
-            res.send(pokemon[i]);
-
+    /*for(i =0; i<pokemon.length;i++){
+        if(pokemon[i].name.toUpperCase==name.toUpperCase){
+            return res.status(200).send(pokemon[i]);
         }
-    }
-    res.status(404);
-    res.send("Pokémon no encontrado");
+    }*/
+
+    const pk= pokemon.filter((p)=>{
+        return (p.name.toUpperCase() == name.toUpperCase()) &&p;
+        //operador ternario: condicion a evaluar? valor si verdadero:valor si falso
+    });
+
+    console.log(pk);
+    (pk.length > 0) ? 
+    res.status(200).send(pk):
+    res.status(404).send("Pokémon no encontrado");
 });
 
 app.listen(process.env.PORT || 3000,()=> {          //3000 puerto, 
