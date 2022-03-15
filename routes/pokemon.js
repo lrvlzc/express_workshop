0eae6e40ee36430dccccb41c7200b15198f4b1f0
vzcl <lvelazquez08@alumnos.uaq.mx>
2022-03-14 22:10:09 -0600

    act 9

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,9 +3,22 @@ const pokemon = express.Router();
 //const pk  = require('../pokedex.json').pokemon;
 const db = require('../config/database');
 
-pokemon.post("/", (req,res,next)=>{
-    return res.status(200).json(req.body);
-})
+pokemon.post("/", async (req,res,next)=>{
+    const {pok_name, pok_height, pok_weight, pok_base_experience} = req.body;
+
+    if(pok_name && pok_height && pok_weight && pok_base_experience ){
+        let query = "INSERT INTO pokemon(pok_name, pok_height, pok_weight, pok_base_experience)";
+        query += `  VALUES('${pok_name}', ${pok_height}, ${pok_weight}, ${pok_base_experience})`;
+
+        const rows = await db.query(query);
+        if(rows.affectedRows ==1 ){
+            return res.status(201).json({code:201, message: "Pokemon insertado"});
+        }
+        
+        return res.status(500).json({code:500, message:"Ocurrio un error"});
+    }
+    return res.status(500).json({code:500, message:"Campos incompletos"});
+});
 
 pokemon.get("/", async (req, res, next) =>{ // "/:name", dos puntos para crear variable
     //console.log(req.params.name);
@@ -17,7 +30,7 @@ pokemon.get(':id([0-9]{1,3})', async (req,res,next)=>{ //([0-9]{1,3})funcion par
     const id = req.params.id;
     if(id>=1 && id <= 722){
         const pkmn= await db.query("SELECT * FROM pokemon WHERE pok_id="+id+";");
-        return res.status(200).json({code:1, message: pkmn});
+        return res.status(200).json({code:200, message: pkmn});
     }
     return res.status(404).send({code: 404, message: "Pokemon no encontrado"});
 });
@@ -39,7 +52,7 @@ pokemon.get('/:name([A-Za-z]+)', async (req, res, next)=>{
 
     if(pkmn.length > 0){
         //const name = req.params.name;
-        return res.status(200).json({code:1, message: pkmn});
+        return res.status(200).json({code:200, message: pkmn});
     }
     return res.status(404).send({code: 404, message: "Pokemon no encontrado"});
 });
