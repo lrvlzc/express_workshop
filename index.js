57fa61c48dbcbd9bf356b49cc29433e3b2bfa285
vzcl <lvelazquez08@alumnos.uaq.mx>
2022-04-04 23:22:08 -0500

    act 14

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ const req = require("express/lib/request");
 const auth = require('./middleware/auth');
 const notFound = require('./middleware/notFound');
 const index = require('./middleware/index');
+const cors = require('./middleware/cors');
 
+app.use(cors);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
